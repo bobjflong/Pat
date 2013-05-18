ed0a638de5f029034e2173fbdd8217c0f783ca9c
Bob Long <robertjflong@gmail.com>
2013-05-18 19:16:28 +0100

    Removing example code from src

--- pat.rb
+++ pat.rb
@@ -40,10 +40,3 @@ class Array
   end
 end
 
-[1,2,3,4,5].pat('full@x:y:z:xs') do |vals|
-  puts vals[:x].to_s
-  puts vals[:y].to_s
-  puts vals[:z].to_s
-  puts vals[:xs].to_s
-  puts vals[:full].to_s
-end
