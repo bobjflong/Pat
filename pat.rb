de215f9d96422ff5e68d898cc80908905ce53988
Bob Long <robertjflong@gmail.com>
2013-05-18 19:16:00 +0100

    Fixes for the x@ pattern bit

--- pat.rb
+++ pat.rb
@@ -9,18 +9,18 @@ class Pattern < Whittle::Parser
   rule(:full) do |r|
     
     r[:name, "@", :full].as { |a, _, rest| {
-        a => Proc.new { |list| list }
+        a => Proc.new { |list, pointer| [pointer, list] }
       }.merge(rest)
     }
 
     r[:name, ":", :name].as { |a, _, b| { 
-        a => Proc.new { |list, pointer| list[pointer]},
-        b => Proc.new { |list, pointer| list[pointer..-1]}
+        a => Proc.new { |list, pointer| [pointer+1, list[pointer + 1]] },
+        b => Proc.new { |list, pointer| [pointer+1, list[pointer + 1..-1]] }
       }
     }
 
     r[:name, ":", :full].as { |a, _, rest| {
-        a => Proc.new { |list, pointer| list[pointer]}
+        a => Proc.new { |list, pointer| [pointer+1, list[pointer + 1]] }
       }.merge(rest)
     }
   end
@@ -33,8 +33,17 @@ class Array
     ans = (Pattern.new).parse(pattern)
     pointer = -1
     block.call(ans.merge(ans) do |key, val|
-      pointer += 1
-      val.call(self, pointer)
+      res = val.call(self, pointer)
+      pointer = res[0]
+      res[1]
     end)
   end
+end
+
+[1,2,3,4,5].pat('full@x:y:z:xs') do |vals|
+  puts vals[:x].to_s
+  puts vals[:y].to_s
+  puts vals[:z].to_s
+  puts vals[:xs].to_s
+  puts vals[:full].to_s
 end
--- readme.md
+++ readme.md
@@ -2,10 +2,11 @@ Pat
 ---
 
 ```ruby
-[1,2,3,4,5].pat('x:y:z:xs') do |vals|
+[1,2,3,4,5].pat('full@x:y:z:xs') do |vals|
   puts vals[:x].to_s
   puts vals[:y].to_s
   puts vals[:z].to_s
   puts vals[:xs].to_s
+  puts vals[:full].to_s
 end
 ```
